fa4650bfc405f5b4d6a3c63d98e4899e9099b41b
LuoYaoyang <smoooom233@gmail.com>
2023-10-31 16:34:44 +0800

    first try

---
+++ app/src/main/java/com/example/myapplication/DataBase/DBUtil.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.DataBase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBUtil extends SQLiteOpenHelper {
+
+    private static final String DB_NAME="DataBase_Second_hand.db";//命名数据库
+
+    private static SQLiteDatabase db=null;//通过db进行数据库操作
+
+    private static final int VERSION=1;//版本 //每一次对数据库进行操作该数据都会加1
+
+    public DBUtil(Context context){
+        super(context,DB_NAME,VERSION,null);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("PRAGMA foreign_keys = false");
+
+        db.execSQL("drop table if exists users");
+
+        // 创建一个用户表
+        db.execSQL("create table users(s_id varchar(20) primary key," +
+                "s_password varchar(20)," +
+                "s_contact varchar(20)," +
+                "s_address varchar(20))");
+
+        // 创建一个管理员表
+        db.execSQL("create table admins(s_id varchar(20) primary key," +
+                "s_password varchar(20)," +
+                "s_contact varchar(20)," +
+                "s_address varchar(20))");
+
+        db.execSQL("PRAGMA foreign_keys = true");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        onCreate(sqLiteDatabase);
+    }
+}
